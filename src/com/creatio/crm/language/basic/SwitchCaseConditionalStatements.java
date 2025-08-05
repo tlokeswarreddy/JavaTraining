e5be1188e474c750e05795f9809356717c928f41
tlokeswarreddy <lokeshwarareddy61@gmail.com>
2025-08-05 21:41:19 +0530

    ifelse&switchcase

--- src/com/creatio/crm/language/basic/SwitchCaseConditionalStatements.java
+++ src/com/creatio/crm/language/basic/SwitchCaseConditionalStatements.java
@@ -5,6 +5,44 @@ public class SwitchCaseConditionalStatements {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String day = "Sunday";
+
+		switch (day) {
+
+		case "Monday":
+
+			System.out.println("weekday");
+			break;
+		case "Tuesday":
+
+			System.out.println("weekday");
+			break;
+		case "wednesday":
+
+			System.out.println("weekday");
+			break;
+		case "Thursday":
+
+			System.out.println("weekday");
+			break;
+		case "friday":
+
+			System.out.println("TGIF");
+			break;
+		case "Saturday":
+
+			System.out.println("weekend");
+			break;
+		case "Sunday":
+
+			System.out.println("weekend");
+			break;
+
+		default:
+			System.out.println("Invalid");
+
+		}
+
 	}
 
 }
